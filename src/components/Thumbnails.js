5810c602828a0241bea9db2b324845ad9f29cb9b
Roshane Miller <millerroshane44@gmail.com>
2022-07-20 21:24:35 -0400

    removed image from Thumbnails component

--- src/components/Thumbnails.js
+++ src/components/Thumbnails.js
@@ -2,23 +2,23 @@ import React from "react"
 import { nanoid } from "nanoid"
 
 export default function Thumbnails({ clicked, url, handleClick, i, select }) {
+  const thumbnailBg = {
+    backgroundImage: `url(${url})`,
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    width: "20%",
+  }
   return (
     <>
       <div
+        style={thumbnailBg}
         className={!clicked ? "thumb-div" : "clicked-thumb-div"}
         key={nanoid()}
         onClick={() => {
           handleClick(i)
           select(i)
         }}
-      >
-        <img
-          key={nanoid()}
-          className="thumbnail"
-          src={url}
-          alt={`product-img${i}`}
-        />
-      </div>
+      ></div>
     </>
   )
 }
